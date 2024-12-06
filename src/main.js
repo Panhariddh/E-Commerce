e6dbfc2a8e88393bc5485595cf0e24a8923f733f
Chea Oudompanhariddh <cheaodompanhariddh.com>
2024-12-06 14:54:37 +0700

    AnimationImage by Kimhorn

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import router from './router'
 import '@fortawesome/fontawesome-free/css/all.css'; // Font Awesome CSS
 import '@fortawesome/fontawesome-free/js/all.js';  // Font Awesome JS
 
+import 'bootstrap/dist/css/bootstrap.css'
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 const app = createApp(App)
 
 app.use(createPinia())
